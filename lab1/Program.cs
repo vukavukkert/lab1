ae825c4bcf8c02187c221a915c9f97dc9dafb0bf
VukkertAS <vukavukkert@gmail.com>
2022-09-03 12:17:40 +0700

    Завершение урока

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -1,3 +1,18 @@
 ﻿using System;
 
-Console.WriteLine("Hello World!");
+int x, y;
+Console.WriteLine("Введите число X");
+x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число Y");
+y = Convert.ToInt32(Console.ReadLine());
+
+//int z = x;
+//x = y;
+//y = z;
+y = x + y;
+x = y - x;
+y = y - x;
+
+
+Console.WriteLine($"x ={x}, y = {y}");
+Console.ReadKey();
